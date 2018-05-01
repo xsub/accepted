96cf8b16cf6f89e577324493adfd9db3ef08ca29
Pawel Suchanecki <subdcc@gmail.com>
2018-05-01 14:24:50 +0200

    Fixed Makefiles.am, notably fixed unistall-hook in src/Makefile.am.

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ combination of lower and upper case letters is allowed.
 */
 
 #include <config.h>
-#include <libgen.h>,
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
