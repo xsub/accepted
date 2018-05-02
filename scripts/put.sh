c59130c462e7b052719cc3901d0c9b84d3604c4d
Pawel Suchanecki <subdcc@gmail.com>
2018-05-02 17:32:43 +0200

    Clean up function registered; Added scripts/put.sh that automates puting code into repo.

---
+++ scripts/put.sh
@@ -0,0 +1,11 @@
+
+ make && (
+./scripts/indent.sh
+git commit -a
+read -p "Push? "
+./scripts/push.sh
+exit 0
+)
+
+echo "failed to build, will not commit/push"
+exit 1
--- src/main.c
+++ src/main.c
@@ -52,118 +52,157 @@ const int REJECTED = EXIT_FAILURE;
 const char *delims = " ";
 const int FILENAME_MAX_LEN = 32;
 
-int program_type = 0x1337; // means unset
+int program_type = 0x1337;	// means unset
 
 int errors = 0;
 #define REPORT_ERROR(msg) printf("ERROR(%3d) %s\n", errors++, msg)
 
-struct entry {
+struct entry
+{
   char *token;
-  LIST_ENTRY(entry) entries;
-} * np;
+    LIST_ENTRY (entry) entries;
+} *np;
 
-LIST_HEAD(accept_head, entry) accept_list_head;
-LIST_HEAD(reject_head, entry) reject_list_head;
+LIST_HEAD (accept_head, entry) accept_list_head;
+LIST_HEAD (reject_head, entry) reject_list_head;
 
-struct accept_head *accept_list_head_ptr;
-struct rejct_head *reject_list_head_ptr;
+     struct accept_head *accept_list_head_ptr;
+     struct rejct_head *reject_list_head_ptr;
 
 
-struct entry *alloc_element(char *token) {
-  struct entry *new_el_ptr = malloc(sizeof(struct entry));
+     struct entry *alloc_element (char *token)
+{
+  struct entry *new_el_ptr = malloc (sizeof (struct entry));
 
-  if (new_el_ptr) {
-    np->token = token;
-  } else {
-    REPORT_ERROR("malloc error");
-  }
+  if (new_el_ptr)
+    {
+      np->token = token;
+    }
+  else
+    {
+      REPORT_ERROR ("malloc error");
+    }
   return new_el_ptr;
 }
 
-void add_accepted(char *token) {
-  struct entry *new_el_ptr = alloc_element(token);
-  LIST_INSERT_HEAD(accept_list_head_ptr, new_el_ptr, entries);
+void
+add_accepted (char *token)
+{
+  struct entry *new_el_ptr = alloc_element (token);
+  LIST_INSERT_HEAD (accept_list_head_ptr, new_el_ptr, entries);
 }
 
-void add_rejected(char *token) {}
+void
+add_rejected (char *token)
+{
+}
 
-void parse_option(char *t) {
+void
+parse_option (char *t)
+{
   // print version
-  if (!strcmp(t, "--version")) {
-    puts("This is " PACKAGE_STRING ".");
-    exit(EXIT_SUCCESS);
-  }
+  if (!strcmp (t, "--version"))
+    {
+      puts ("This is " PACKAGE_STRING ".");
+      exit (EXIT_SUCCESS);
+    }
 
-  if (!strcmp(t, "--help"))
-  {
-        puts ("Usage: WIP");
-        exit(EXIT_SUCCESS);
-  }
+  if (!strcmp (t, "--help"))
+    {
+      puts ("Usage: WIP");
+      exit (EXIT_SUCCESS);
+    }
 }
 
-int main(int argc, char **argv) {
+void
+cleanup (void)
+{
+  while (accept_list_head.lh_first != NULL)
+    LIST_REMOVE (accept_list_head.lh_first, entries);
+  while (reject_list_head.lh_first != NULL)
+    LIST_REMOVE (reject_list_head.lh_first, entries);
+#if DEBUG_ON
+  puts ("Cleanup done.");
+#endif
+}
+
+int
+main (int argc, char **argv)
+{
 
   // The operation mode (program_type) depends on binary name: check the
   //
   // actual file name
   char binary_name[FILENAME_MAX_LEN];
-  strncpy(binary_name, basename(argv[0]), FILENAME_MAX_LEN);
+  strncpy (binary_name, basename (argv[0]), FILENAME_MAX_LEN);
 
-  if (!strcmp(binary_name, "accepted")) {
-    program_type = ACCEPTED;
-  } else if (!strcmp(binary_name, "rejected")) {
-    program_type = REJECTED;
-  }
+  if (!strcmp (binary_name, "accepted"))
+    {
+      program_type = ACCEPTED;
+    }
+  else if (!strcmp (binary_name, "rejected"))
+    {
+      program_type = REJECTED;
+    }
 
 #if DEBUG_ON
-  printf("Program type (operation mode): %s\n",
-         (ACCEPTED == program_type) ? "accepted" : "rejected");
+  printf ("Program type (operation mode): %s\n",
+	  (ACCEPTED == program_type) ? "accepted" : "rejected");
 #endif
 
   // no args provided, no answer can be accepting
-  if (argc < 2) {
-    if (ACCEPTED == program_type) {
-      exit(REJECTED);
-    } else {
-      exit(ACCEPTED);
+  if (argc < 2)
+    {
+      if (ACCEPTED == program_type)
+	{
+	  exit (REJECTED);
+	}
+      else
+	{
+	  exit (ACCEPTED);
+	}
     }
-  }
 
 
-  /* Init data */ 
-  LIST_INIT(&accept_list_head);
-  LIST_INIT(&reject_list_head);
+  /* Init list data */
+  LIST_INIT (&accept_list_head);
+  LIST_INIT (&reject_list_head);
+  /* register cleanup */
+  atexit (cleanup);
+
 
   /*
    * walk through tokens
    */
   short arg_id = 1;
-  while (arg_id < argc) {
-    char *token = argv[arg_id++];
+  while (arg_id < argc)
+    {
+      char *token = argv[arg_id++];
 
 #if DEBUG_ON
-    printf("Processing token: '%s'\n", token);
-#endif 
+      printf ("Processing token: '%s'\n", token);
+#endif
 
-    if ('+' == token[0]) {
-      add_accepted(argv[arg_id]);
-    } else if ('-' == token[0] && '-' != token[1]) {
-      add_rejected(argv[arg_id]);
-    } else {
-      parse_option(token);
+      if ('+' == token[0])
+	{
+	  add_accepted (argv[arg_id]);
+	}
+      else if ('-' == token[0] && '-' != token[1])
+	{
+	  add_rejected (argv[arg_id]);
+	}
+      else
+	{
+	  parse_option (token);
+	}
     }
-  }
 
 cleanup:
   /*
    * Delete lists
    */
-  while (accept_list_head.lh_first != NULL)
-    LIST_REMOVE(accept_list_head.lh_first, entries);
-  while (reject_list_head.lh_first != NULL)
-    LIST_REMOVE(reject_list_head.lh_first, entries);
 
-  exit(REJECTED);
+  exit (REJECTED);
 }
 
 /*
